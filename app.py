d666f24b4ec5dc724f586e1eb746129d045008fb
LIN Loris <loris.lin@epita.fr>
2024-10-24 18:24:47 +0200

    feat: t5 model with classication of text

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-import joblib
-
-model = joblib.load("regression.joblib")
-class HouseData(BaseModel):
-    size: float
-    nb_rooms: int
-    garden: int
+from transformers import T5ForConditionalGeneration, T5Tokenizer
+import torch
 
 app = FastAPI()
 
-@app.post("/predict")
-def predict(data: HouseData):
-    features = [[data.size, data.nb_rooms, data.garden]]
-    prediction = model.predict(features)
-    return {"predicted_price": prediction[0]}
+model_name = "t5-small" 
+model = T5ForConditionalGeneration.from_pretrained(model_name)
+tokenizer = T5Tokenizer.from_pretrained(model_name, legacy=False)
 
+model.eval()
+
+class TextInput(BaseModel):
+    text: str
+
+@app.post("/classify/")
+async def classify(input: TextInput):
+    with torch.no_grad():
+        enc = tokenizer("sst2 sentence: "+ input.text, return_tensors="pt")
+        decoder_input_ids = torch.tensor([tokenizer.pad_token_id]).unsqueeze(0) 
+        logits = model(**enc, decoder_input_ids=decoder_input_ids)[0]
+        tokens = torch.argmax(logits, dim=2)
+        sentiments = tokenizer.batch_decode(tokens)
+        return {"classification": sentiments}
